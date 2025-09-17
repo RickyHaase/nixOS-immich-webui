e5d46389217203223e9b04be0e1626b22a29d2cd
Ricky Haase <85689086+RickyHaase@users.noreply.github.com>
2025-09-16 23:35:11 -0400

    add example config files
    Config files in the state that they were last time I was working on this
    project. Need to get them into the repo so that I can track changes and
    include for context in both vibe-coding and project documentation

---
+++ example/etc/nixos/immich.nix
@@ -0,0 +1,39 @@
+{ config, pkgs, ... }:
+
+{
+  virtualisation.docker.enable = true;
+  virtualisation.docker.autoPrune.enable = true;
+  virtualisation.docker.autoPrune.dates = "monthly";
+
+  environment.systemPackages = with pkgs; [
+    wget
+  ];
+
+  users.groups.immich = {};
+  users.users.immich = {
+    isSystemUser = true;
+    group = "immich";
+    description = "Immich";
+    extraGroups = [ "docker" ];
+  };
+
+  systemd.services.immich-app = {
+    description = "Manage Immich Compose Stack";
+    requires = [ "docker.service" ];
+    after = [ "docker.service" ];
+    wantedBy = [ "multi-user.target" ];
+
+    serviceConfig = {
+      Type = "simple";
+      ExecStart = "${pkgs.docker}/bin/docker compose up";
+      ExecStop = "${pkgs.docker}/bin/docker compose down";
+      Restart = "always";
+      WorkingDirectory = "/root/immich-app";
+      TimeoutStopSec = "90";
+    };
+  };
+
+  # Need to define a cron job to update Immich
+  # Enable/disable and configure time in webGUI
+
+}
